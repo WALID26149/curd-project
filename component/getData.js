9c8bb06c9eec8697085c4288422c08f5154508ab
walid allane <walid.allane@outlook>
2023-04-22 12:34:30 +0100

    make the crud operation work

--- component/getData.js
+++ component/getData.js
@@ -2,32 +2,16 @@ const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser')
 const ejs = require('ejs');
-const { json } = require('body-parser');
 const Product = require('../models/ProductsDB.js');
 
 
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set("view engine", "ejs");
 
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 > products.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  next();
-};
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price'
-    });
-  }
-  next();
-};
 
 // get all product
 exports.getAllProducts = async(req, res) => {
@@ -48,59 +32,78 @@ exports.getAllProducts = async(req, res) => {
 };
 
 // get product
-exports.getProduct = (req, res) => {
-  const id = req.params.id * 1;
+exports.getProduct = async(req, res) => {
+  try {
+    const product = await Product.findById(req.params.id).exec();
 
-  const product = products.find(el => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      product
-    }
-  });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        product
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 // post product
-exports.postProduct = (req, res) => {
-  const newId = products[products.length - 1].id + 1;
-  const newProduct = Object.assign({ id: newId }, req.body);
+exports.postProduct = async(req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
 
-  products.push(newProduct);
-
-  fs.writeFile(
-    `${__dirname}/../dataBase/productData.json`,
-    JSON.stringify(products),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          product: newProduct
-        }
-      });
-    }
-  );
-  // const url ="https://dummyjson.com/products";
-
-  //   https.get(url, function(response) {
-  //     response.on("data", function(data) {
-  //       const productData = JSON.parse(data)
-  //       const title = productData.products[0].title;
-  //       res.render('homePage', {product: `${title}`});
-  //     });
-  //   });
+    res.status(201).json({
+      status: 'success',
+      data: {
+        product: newProduct
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 // patch product
-exports.patchProduct = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+exports.updateProduct = async(req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        product
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 // delete product
-exports.deleteProduct = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+exports.deleteProduct = async(req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
--- router/userProduct.js
+++ router/userProduct.js
@@ -3,17 +3,18 @@ const getProductData = require('./../component/getData.js');
 
 const router = express.Router();
 
-router.param('id', getProductData.checkID);
+// router.param('id', getProductData.checkID);
 
 router
   .route('/')
    .get(getProductData.getAllProducts)
-   .post(getProductData.checkBody, getProductData.postProduct);
+   .post(getProductData.postProduct)
+  //  .post(getProductData.checkBody, getProductData.postProduct);
 
 router
   .route('/:id')
    .get(getProductData.getProduct)
-   .patch(getProductData.patchProduct)
+   .patch(getProductData.updateProduct)
    .delete(getProductData.deleteProduct)
 
 module.exports = router;
